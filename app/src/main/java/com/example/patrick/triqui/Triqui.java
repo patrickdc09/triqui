f10054689d512e23a4bfdd3f64e5df3cc754d225
Cristian Gomez <cristianfgr@gmail.com>
2015-10-14 20:52:19 -0500

    logica para solucionar el triqui

---
+++ app/src/main/java/com/example/patrick/triqui/Triqui.java
@@ -0,0 +1,97 @@
+package com.example.patrick.triqui;
+
+/**
+ * Created by cristiangomez on 14/10/15.
+ */
+public class Triqui {
+
+    private int[][] tablero;
+    private boolean activo;
+
+    public Triqui() {
+        tablero = new int[3][3];
+        activo = true;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablero[i][j] = 0;
+            }
+        }
+
+    }
+
+    public void marcar(int marca, int i, int j) {
+        tablero[i][j] = marca;
+    }
+
+    public boolean estaMarcada(int i, int j) {
+        if (tablero[i][j] != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int hayganador() {
+        int valorFila0 = chequearFila(0);
+
+        if (valorFila0 != 0) {
+            activo = false;
+            return valorFila0;
+        }
+
+        int valorFila1 = chequearFila(1);
+
+        if (valorFila1 != 0) {
+            activo = false;
+            return valorFila1;
+        }
+
+        int valorFila2 = chequearFila(2);
+
+        if (valorFila2 != 0) {
+            activo = false;
+            return valorFila2;
+        }
+
+        int valorColumna0 = chequearColumna(0);
+
+        if (valorColumna0 != 0) {
+            activo = false;
+            return valorColumna0;
+        }
+
+        int valorColumna1 = chequearColumna(1);
+
+        if (valorColumna1 != 0) {
+            activo = false;
+            return valorColumna1;
+        }
+
+        int valorColumna2 = chequearColumna(2);
+
+        if (valorColumna2 != 0) {
+            activo = false;
+            return valorColumna2;
+        }
+        return 0;
+    }
+
+    private int chequearFila(int i) {
+        if (tablero[i][0] == tablero[i][1] && tablero[i][0] == tablero[i][2]) {
+            return tablero[i][0];
+        }
+        return 0;
+    }
+
+    private int chequearColumna(int i) {
+        if (tablero[0][i] == tablero[1][i] && tablero[0][i] == tablero[2][i]) {
+            return tablero[0][i];
+        }
+        return 0;
+    }
+
+    public boolean esActivo() {
+        return activo;
+    }
+}
--- app/src/main/java/com/example/patrick/triqui/TriquiActivity.java
+++ app/src/main/java/com/example/patrick/triqui/TriquiActivity.java
@@ -6,13 +6,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
 public class TriquiActivity extends AppCompatActivity {
 
     private int contador = 0;
+    private Triqui triqui;
+    private Button boton1;
+    private Button boton2;
+    private Button boton3;
+    private Button boton4;
+    private Button boton5;
+    private Button boton6;
+    private Button boton7;
+    private Button boton8;
+    private Button boton9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,27 +29,117 @@ public class TriquiActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        boton1 = (Button) findViewById(R.id.boton_1);
+        boton2 = (Button) findViewById(R.id.boton_2);
+        boton3 = (Button) findViewById(R.id.boton_3);
+        boton4 = (Button) findViewById(R.id.boton_4);
+        boton5 = (Button) findViewById(R.id.boton_5);
+        boton6 = (Button) findViewById(R.id.boton_6);
+        boton7 = (Button) findViewById(R.id.boton_7);
+        boton8 = (Button) findViewById(R.id.boton_8);
+        boton9 = (Button) findViewById(R.id.boton_9);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reiniciarJuego();
+            }
+        });
+
+
+        triqui = new Triqui();
+    }
+
+    private void reiniciarJuego() {
+        boton1.setText("");
+        boton2.setText("");
+        boton3.setText("");
+        boton4.setText("");
+        boton5.setText("");
+        boton6.setText("");
+        boton7.setText("");
+        boton8.setText("");
+        boton9.setText("");
+        triqui = new Triqui();
     }
 
 
     public void laviejaListener(View view) {
         Button botonPresionado = (Button) view;
 
+        if (triqui.esActivo() == true) {
+            jugar(botonPresionado);
+        }
+    }
+
+    private void jugar(Button botonPresionado) {
         if (botonPresionado.getText().toString().equals("")) {
             contador++;
             String resultado;
+
+            int i = -1;
+            int j = -1;
+            int marca;
+
+            switch (botonPresionado.getId()) {
+                case R.id.boton_1:
+                    i = 0;
+                    j = 0;
+                    break;
+                case R.id.boton_2:
+                    i = 0;
+                    j = 1;
+                    break;
+                case R.id.boton_3:
+                    i = 0;
+                    j = 2;
+                    break;
+                case R.id.boton_4:
+                    i = 1;
+                    j = 0;
+                    break;
+                case R.id.boton_5:
+                    i = 1;
+                    j = 1;
+                    break;
+                case R.id.boton_6:
+                    i = 1;
+                    j = 2;
+                    break;
+                case R.id.boton_7:
+                    i = 2;
+                    j = 0;
+                    break;
+                case R.id.boton_8:
+                    i = 2;
+                    j = 1;
+                    break;
+                case R.id.boton_9:
+                    i = 2;
+                    j = 2;
+                    break;
+            }
+
+
             if (contador % 2 == 0) {
                 resultado = "turno x";
                 botonPresionado.setText("O");
-
+                marca = 1;
 
             } else {
                 resultado = "turno O";
                 botonPresionado.setText("X");
+                marca = 2;
+            }
+            triqui.marcar(marca, i, j);
+
+            if (triqui.hayganador() != 0) {
+                Snackbar.make(botonPresionado, "Hay ganador!", Snackbar.LENGTH_SHORT).show();
+            } else {
+                Snackbar.make(botonPresionado, resultado, Snackbar.LENGTH_SHORT).show();
             }
 
-            Snackbar.make(view, resultado, Snackbar.LENGTH_SHORT).show();
         }
-
     }
 }
